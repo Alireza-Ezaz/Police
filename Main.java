507fffbcf1c583197612fb69ec722f2fa9fa9e44
Alireza <sar.ezaz@aut.ac.ir>
2019-04-15 17:49:00 +0430

    Starting main

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        final Scanner scan = new Scanner(System.in);
+        int length;
+        int width;
+        int policeNumbers;
+        System.out.println("Enter length and width for the court dimensions");
+        length = scan.nextInt();
+        width = scan.nextInt();
+        System.out.println("Enter the number of polices");
+        policeNumbers = scan.nextInt();
+
+        Court playGround = new Court(length,width);
+
+
+
     }
 }
