a04c4a8a21f0cecb439f7104f2765852bd931e41
Alireza <sar.ezaz@aut.ac.ir>
2019-04-19 16:32:57 +0430

    Police random move added

--- Police.java
+++ Police.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Police {
     private int lastx;
     private int lasty;
@@ -45,5 +47,87 @@ public class Police {
     public int getLasty() {
         return lasty;
     }
+
+    public void moveRandomly(int length,int width){
+        Random randomGenerator = new Random();
+
+        if (currentx != 0 && currentx != length - 1 && currenty != 0 && currenty != width - 1) {
+            int rx = randomGenerator.nextInt(3) - 1;
+            int ry = randomGenerator.nextInt(3) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+
+        if (currentx == 0 && currenty != 0 && currenty != width - 1) {
+            int rx = randomGenerator.nextInt(2);
+            int ry = randomGenerator.nextInt(3) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currenty == 0 && currentx != 0 && currentx != length - 1) {
+            int rx = randomGenerator.nextInt(3) - 1;
+            int ry = randomGenerator.nextInt(2);
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+
+        }
+        if (currentx == length - 1 && currenty != 0 && currenty != width - 1) {
+            int rx = randomGenerator.nextInt(2) - 1;
+            int ry = randomGenerator.nextInt(3) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currenty == width - 1 && currentx != 0 && currentx != length - 1) {
+            int rx = randomGenerator.nextInt(3) - 1;
+            int ry = randomGenerator.nextInt(2) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currentx == 0 && currenty == 0) {
+            int rx = randomGenerator.nextInt(2);
+            int ry = randomGenerator.nextInt(2);
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currentx == length - 1 && currenty == 0) {
+            int rx = randomGenerator.nextInt(2) - 1;
+            int ry = randomGenerator.nextInt(2);
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currentx == 0 && currenty == width - 1) {
+            int rx = randomGenerator.nextInt(2);
+            int ry = randomGenerator.nextInt(2) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+        if (currentx == length - 1 && currenty == width - 1) {
+            int rx = randomGenerator.nextInt(2) - 1;
+            int ry = randomGenerator.nextInt(2) - 1;
+            lastx = currentx;
+            lasty = currenty;
+            currentx = currentx + rx;
+            currenty = currenty + ry;
+        }
+
+
+    }
+
 
 }
