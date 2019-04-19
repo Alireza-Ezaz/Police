e94ad42035c0de183314b71a0c99571acc5b173c
Alireza <sar.ezaz@aut.ac.ir>
2019-04-19 12:32:40 +0430

    Random for placing the thief added

--- Court.java
+++ Court.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Court {
     private int length;
     private int width;
-    ArrayList<Police> polices = new ArrayList<Police>();
-    ArrayList<Thief> thieves = new ArrayList<Thief>();
+    private ArrayList<Police> polices = new ArrayList<Police>();
+    private Thief thief;
 
     private String[][] court;
 
@@ -24,16 +24,8 @@ public class Court {
         for (int i = 0; i < policeNumbers; i++) {
             int x = randomGenator.nextInt(length);
             int y = randomGenator.nextInt(width);
-            /*
-            if (polices.size() == 0) {
-                court[x][y] = "P";
-                Police p = new Police(x, y);
-                polices.add(p);
-                System.out.println("Random created for police " + i);
-                continue;
-            }*/ else {
-                boolean bl = true;
-
+            boolean bl = true;
+            if (polices.size() != 0) {
                 for (Police police : polices) {
                     if (police.getCurrentx() == x && police.getCurrenty() == y) {
                         i--;
@@ -42,24 +34,42 @@ public class Court {
                         break;
                     }
                 }
-                if(bl == true){
-                    court[x][y] = "P";
+            }
+            if (bl == true) {
+                court[x][y] = "P";
                 Police p = new Police(x, y);
                 polices.add(p);
                 System.out.println("Random created for police " + i);
-                }
-
-
-
-
-
             }
+
 
         }
 
     }
+    //We call this method after placing polices
+
+    public void placeThiefRandomly() {
+        Random randomGenrator = new Random();
+        while (true) {
+            int x = randomGenrator.nextInt(length);
+            int y = randomGenrator.nextInt(width);
+            boolean b = true;
+            for (Police police : polices) {
+                if (police.getCurrentx() == x && police.getCurrenty() == y)
+                    b = false;
+            }
+            if(b == true){
+                thief = new Thief(x,y);
+                court[x][y] = "D";
+                System.out.println("Random created for the thief " );
+                break;
+            }
+        }
+    }
+
 
 }
+
 
 // public void placeThief
 
