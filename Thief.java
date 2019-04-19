c3e62e659a8f2f9fa26beddef3832c6097662394
Alireza <sar.ezaz@aut.ac.ir>
2019-04-19 12:03:59 +0430

    setter and getter added in Thief class

--- Thief.java
+++ Thief.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class Thief {
+    private int currentx;
+    private int currenty;
+    private int moveNumbers = 0;
+
+    public  Thief(int currentx, int currenty){
+        this.currentx = currentx;
+        this.currenty = currenty;
+    }
+
+    public void setCurrentx(int currentx) {
+        this.currentx = currentx;
+    }
+
+    public void setCurrenty(int currenty) {
+        this.currenty = currenty;
+    }
+
+    public void setMoveNumbers(int moveNumbers) {
+        this.moveNumbers = moveNumbers;
+    }
+
+    public int getCurrentx() {
+        return currentx;
+    }
+
+    public int getCurrenty() {
+        return currenty;
+    }
+
+    public int getMoveNumbers() {
+        return moveNumbers;
+    }
 }
